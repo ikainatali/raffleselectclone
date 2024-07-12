58a02a181e08356bac8bde5e7ee57b12df799b90
ikainatali <ikainatali@outlook.com>
2024-07-12 16:45:14 +0500

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/raffleselectclone/",
   plugins: [react()],
-  base: "/vite-deploy/",
 });
